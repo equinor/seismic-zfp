c93f555cf917cc73f996cd90c81c9a4db98f4e7e
David Wade <dawad@equinor.com>
2019-10-28 14:42:49 +0000

    Convert files which don't fit in memory

--- seismic_zfp/convert.py
+++ seismic_zfp/convert.py
@@ -11,6 +11,9 @@ def convert_segy(in_filename, out_filename, bits_per_voxel=4, method="InMemory")
     if method == "InMemory":
         print("Converting: In={}, Out={}".format(in_filename, out_filename))
         convert_segy_inmem(in_filename, out_filename, bits_per_voxel)
+    elif method == "Stream":
+        print("Converting: In={}, Out={}".format(in_filename, out_filename))
+        convert_segy_stream(in_filename, out_filename, bits_per_voxel)
     else:
         raise NotImplementedError("So far can only convert SEG-Y files which fit in memory")
 
@@ -32,3 +35,37 @@ def convert_segy_inmem(in_filename, out_filename, bits_per_voxel):
     t3 = time.time()
 
     print("Total conversion time: {}, of which read={}, compress={}, write={}".format(t3-t0, t1-t0, t2-t1, t3-t2))
+
+
+def convert_segy_stream(in_filename, out_filename, bits_per_voxel):
+    t0 = time.time()
+
+    with segyio.open(in_filename) as segyfile:
+
+        test_slice = segyfile.iline[segyfile.ilines[0]]
+        trace_length = test_slice.shape[1]
+        n_xlines = len(segyfile.xlines)
+        n_ilines = len(segyfile.ilines)
+
+        padded_shape = (pad(n_ilines, 4), pad(n_xlines, 4), pad(trace_length, 2048 // bits_per_voxel))
+
+        print("Trace length={}, n_xlines={}, n_ilines={}, padded_shape={}".format(trace_length, n_xlines, n_ilines, padded_shape))
+
+        segy_buffer = np.zeros((4, padded_shape[1], padded_shape[2]), dtype=np.float32)
+
+        with open(out_filename, 'wb') as f:
+            for plane_set_id in range(padded_shape[0] // 4):
+                if (plane_set_id+1)*4 > n_ilines:
+                    planes_to_read = n_ilines % 4
+                    segy_buffer = np.zeros((4, padded_shape[1], padded_shape[2]), dtype=np.float32)
+                else:
+                    planes_to_read = 4
+                for i in range(planes_to_read):
+                    data = np.asarray(segyfile.iline[segyfile.ilines[plane_set_id*4 + i]])
+                    segy_buffer[i, 0:n_xlines, 0:trace_length] = data
+
+                compressed = compress(segy_buffer, rate=bits_per_voxel)
+                f.write(compressed)
+
+    t3 = time.time()
+    print("Total conversion time: {}".format(t3-t0))
