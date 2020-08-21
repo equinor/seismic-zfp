d6a83ae13e5b9d8c2af10ccab2a44f58d0d3a9ab
David Wade <dawad@equinor.com>
2020-08-21 12:46:36 +0000

    Test correct ilines/xlines lists from ZGY

--- tests/test_compress.py
+++ tests/test_compress.py
@@ -33,8 +33,13 @@ def compress_and_compare_zgy(zgy_file, sgy_file, tmp_path, bits_per_voxel, rtol)
 
     with SgzReader(out_sgz) as reader:
         sgz_data = reader.read_volume()
+        sgz_ilines = reader.ilines
+
+    with segyio.open(sgy_file) as f:
+        ref_ilines = f.ilines
 
     assert np.allclose(sgz_data, segyio.tools.cube(sgy_file), rtol=rtol)
+    assert all([a == b for a, b in zip(sgz_ilines, ref_ilines)])
 
 
 def test_compress_zgy8(tmp_path):
