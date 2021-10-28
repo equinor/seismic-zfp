4642d121377bad385a7222b2935b731de9061b46
dawad <dawad@statoil.com>
2021-10-28 12:53:35 +0200

    Skip xarray test if xarray not installed

--- tests/test_xarray.py
+++ tests/test_xarray.py
@@ -1,7 +1,13 @@
-import xarray as xr
+import pytest
+
+try:
+    import xarray as xr
+except ImportError:
+    xr = None
 
 SGZ_FILE = 'test_data/small_4bit.sgz'
 
+@pytest.mark.skipif(xr is None, reason="Requires xarray")
 def test_xarray_from_sgz_file():
     s = xr.open_dataset(SGZ_FILE)
     arr = s.data[0:1, 0:1, 0:5].to_numpy()
