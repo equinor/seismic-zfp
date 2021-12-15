5bd3ba88bdb15dfc6e331574a734518daf94e402
dawad <dawad@statoil.com>
2021-12-15 20:13:25 +0100

    Expand azure example

--- examples/sgz_reading/read-from-azure.py
+++ examples/sgz_reading/read-from-azure.py
@@ -1,22 +1,29 @@
 import seismic_zfp
 import time
 
-sas = None
-remote_file = (sas, "testseismic", "Volve.sgz")
+sas_tokens = {'premiumblock': None, 'premiumpage' : None, 'hotblock': None}
 
-f = seismic_zfp.open(remote_file)
+for storage_type, sas_token in sas_tokens.items():
+    print(storage_type)
+    remote_file = (sas_token, "testseismic", "Volve.sgz")
 
-print("Trace shape\n", f.trace[1337].shape)
+    f = seismic_zfp.open(remote_file)
 
-print("File text header\n", f.text[0])
-print("File binary header\n", f.bin)
+    print("Trace shape\n", f.trace[1337].shape)
 
-print("Trace header #42\n", f.header[42])
+    print("File text header\n", f.text[0])
+    print("File binary header\n", f.bin)
 
-t0 = time.time()
-data = f.iline[f.ilines[0]]
-print("Inline took", time.time()-t0)
+    print("Trace header #42\n", f.header[42])
 
-t0 = time.time()
-data = f.xline[f.xlines[0]]
-print("Crossline took", time.time()-t0)
+    t0 = time.time()
+    data = f.iline[f.ilines[0]]
+    print("Inline took", time.time()-t0)
+
+    t0 = time.time()
+    data = f.xline[f.xlines[0]]
+    print("Crossline took", time.time()-t0)
+
+    t0 = time.time()
+    data = f.depth_slice[200]
+    print("Zslice took", time.time()-t0)
