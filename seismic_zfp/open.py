6ac56a23ab98b857971687a4765086eb49739957
David Wade <dawad@equinor.com>
2022-08-03 13:22:25 +0200

    Argument should not be positional

--- seismic_zfp/open.py
+++ seismic_zfp/open.py
@@ -4,4 +4,4 @@ from .segyio_emulator import SegyioEmulator
 def open(filename, mode='r', chunk_cache_size: int = None):
     assert (mode == 'r')
     assert (isinstance(chunk_cache_size, int) or chunk_cache_size is None)
-    return SegyioEmulator(filename, chunk_cache_size)
+    return SegyioEmulator(filename, chunk_cache_size=chunk_cache_size)
